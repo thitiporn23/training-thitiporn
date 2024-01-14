28a667c5c32cc9a27762449b76fe5154550d4f5f
thitiporn23 <thitipornwichianrat@gmail.com>
2024-01-14 14:24:33 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>หน้าหลัก</h1><hr />
+      <center><img src="nvc.jpg" alt="" /></center>
+    </div>
+  )
+}
+
+export default Home
